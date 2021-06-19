1530c0e165613c1d5408602e20fb306eb414f651
tomliu0793 <181654686@qq.com>
2021-06-19 16:59:41 +0800

    Update light_colors.dart

--- lib/theme/colors/light_colors.dart
+++ lib/theme/colors/light_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LightColors  {
+class LightColors {
   static const Color kLightYellow = Color(0xFFFFF9EC);
   static const Color kLightYellow2 = Color(0xFFFFE4C7);
   static const Color kDarkYellow = Color(0xFFF9BE7C);
